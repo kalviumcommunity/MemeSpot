5531337eeea0d2409210d31a6ddef67dc26b91eb
Vighnesh <vighneshzagade4@gmail.com>
2023-06-29 10:23:33 +0530

    Initialized a React App

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> Hey Client! This is the frontend part of the webpage. </h1>
+    </div>
+  );
+}
+
+export default App;
