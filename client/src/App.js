14635afac7a25adec8b7b3f7d3c0c6988cf90884
Vighnesh <vighneshzagade4@gmail.com>
2023-06-29 22:39:53 +0530

    Created Components and Added Search Bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import TopVector from './images/Top Vector.png'
+import BottomVector from './images/Bottom Vector.png'
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-     <h1> Hey Client! This is the frontend part of the webpage. </h1>
+     <div className='circles' style={{top: '0%', right: '0'}}><img src={TopVector} alt='TopCircle' className='circles'/></div>
+     <div className='circles' style={{bottom: '0%', left: '0'}}><img src={BottomVector} alt='BottomCircle' className='circles'/></div>
+     <Home/>
     </div>
   );
 }
---
+++ client/src/components/ProfileSection/ProfileSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ProfileSection.css'
+import LogoSearch from '../LogoSearch/LogoSearch.jsx'
+
+const ProfileSection = () => {
+  return (
+    <div className='profileSection'>
+        <LogoSearch/>
+    </div>
+  )
+}
+
+export default ProfileSection
