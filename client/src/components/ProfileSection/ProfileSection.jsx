5b42c1f5eb89f95a77213f6fee7ed5b7053b10db
Vighnesh <vighneshzagade4@gmail.com>
2023-06-29 23:54:10 +0530

    Added the Profile Card

--- client/src/components/ProfileSection/ProfileSection.jsx
+++ client/src/components/ProfileSection/ProfileSection.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './ProfileSection.css'
 import LogoSearch from '../LogoSearch/LogoSearch.jsx'
+import ProfileCard from '../ProfileCard/ProfileCard'
 
 const ProfileSection = () => {
   return (
     <div className='profileSection'>
         <LogoSearch/>
+        <ProfileCard/>
     </div>
   )
 }
