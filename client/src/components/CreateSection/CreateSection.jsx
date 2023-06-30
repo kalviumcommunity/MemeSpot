28106f53bb18afa409dfb9dee2b9da481036d97c
Vighnesh <vighneshzagade4@gmail.com>
2023-06-30 20:01:03 +0530

    Added Create Section

---
+++ client/src/components/CreateSection/CreateSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './CreateSection.css'
+import FollowersCard from '../FollowersCard/FollowersCard'
+
+const CreateSection = () => {
+  return (
+    <div>   
+        <FollowersCard/>
+    </div>
+  )
+}
+
+export default CreateSection
