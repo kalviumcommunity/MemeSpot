03aad6c2197ddca0318892c1eb88c35e4e124606
Vighnesh <vighneshzagade4@gmail.com>
2023-06-30 22:57:25 +0530

    Added a CreateCard

--- client/src/components/CreateSection/CreateSection.jsx
+++ client/src/components/CreateSection/CreateSection.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './CreateSection.css'
 import FollowersCard from '../FollowersCard/FollowersCard'
+import UploadFileCard from '../UploadFileCard/UploadFileCard'
 
 const CreateSection = () => {
   return (
     <div>   
+        <UploadFileCard/> 
         <FollowersCard/>
     </div>
   )
