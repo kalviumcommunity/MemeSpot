56680b3316e07505d43d1024d3595d24a358c79f
Vighnesh <vighneshzagade4@gmail.com>
2023-07-01 19:07:04 +0530

    Minor Changes in Profile Section

---
+++ client/src/components/NavigationButtons/NavigationButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './NavigationButton.css'
+
+const NavigationButton = () => {
+  return (
+    <div className='navigationButtons'>
+        <button>Home</button>
+        <button>Chats</button>
+    </div>
+  )
+}
+
+export default NavigationButton
