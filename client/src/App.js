1a7d273d065ebcc499a39cdb1d1a2dd17b111c24
Vighnesh <vighneshzagade4@gmail.com>
2023-07-12 16:49:43 +0530

    Created Auth Page for Login/SignUp

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import TopVector from './images/Top Vector.png'
 import BottomVector from './images/Bottom Vector.png'
 // import Home from './pages/Home/Home';
-import Profile from './pages/Profile/Profile';
+// import Profile from './pages/Profile/Profile';
+import Auth from './pages/Auth/Auth';
 
 function App() {
   return (
     <div className="App">
-     <div className='circles' style={{top: '0%', right: '0'}}><img src={TopVector} alt='TopCircle' className='circles'/></div>
-     <div className='circles' style={{bottom: '0%', left: '0'}}><img src={BottomVector} alt='BottomCircle' className='circles'/></div>
-     {/* <Home/> */}
-     <Profile/>
+      <div className='circles' style={{top: '0%', right: '0'}}><img src={TopVector} alt='TopCircle' className='circles'/></div>
+      <div className='circles' style={{bottom: '0%', left: '0'}}><img src={BottomVector} alt='BottomCircle' className='circles'/></div>
+      {/* <Home/> */}
+      {/* <Profile/> */}
+      <Auth/>
     </div>
   );
 }
---
+++ client/src/pages/Auth/Auth.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import './Auth.css'
+import Logo from '../../images/Logo.png'
+import { Button, TextField } from '@mui/material'
+
+const Auth = () => {
+    function SignUp() {
+        return(
+            <div className='loginContainer'>
+                <h3>SignUp</h3>
+
+                <div className='textFieldContainer'>
+                    <div className='lineUpTextBox'>
+                        <TextField variant='filled' label='FirstName' className='textfield'/>
+                        <TextField variant='filled' label='LastName' className='textfield'/>
+                    </div>
+                        <TextField variant='filled' label='UserName' className='textfield'/>
+                    <div className='lineUpTextBox'>
+                        <TextField variant='filled' label='Password' className='textfield' type='password'/>
+                        <TextField variant='filled' label='Confirm Password' className='textfield' type='password'/>
+                    </div>
+                </div>
+
+                <div className='signUpSuggestion'>
+                    <span>
+                        Already have an account ? Login
+                    </span>
+                    <Button className='loginButton'>SignUp</Button>
+                </div>
+            </div>
+        )
+    }
+    function Login() {
+        return(
+            <div className='loginContainer'>
+                <h3>Login</h3>
+
+                <div className='inputBoxes'>
+                    <TextField variant='filled' label='Username' className='inputBox'/>
+                    <TextField variant='filled' label='Password' className='inputBox' type='password'/>
+                </div>
+
+                <div className='signUpSuggestion'>
+                    <span>
+                        Don't have an account ? SignUp. 
+                    </span>
+                    <Button className='loginButton'>Login</Button>
+                </div>
+            </div>
+        )
+    }
+return (
+    <div className='Auth'>
+        {/* <Login/> */}
+        <SignUp/>
+        <div className='names'>
+            <img src={Logo} alt="Logo" />
+            <div className='heading'>
+                <h1>MemeSpot</h1>
+                <h6>Welcome to the world of Memers !</h6>
+            </div>
+        </div>
+    </div>
+)
+}
+
+export default Auth
