aebc4ee59cddb9fc0b7489df35ad201446207791
Vighnesh <vighneshzagade4@gmail.com>
2023-07-13 20:31:17 +0530

    Implemented updateUser functionality

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -16,3 +16,17 @@ export const getUser = async(req, res) => {
         res.status(500).json({Message: error.message})
     }
 }
+
+export const updateUser = async(req, res) => {
+    const id = req.params.id
+    const {currentUserId, currentUserAdminStatus, password} = req.body
+
+    if(id === currentUserId || currentUserAdminStatus){
+        try {
+            const user = await UserModel.findByIdAndUpdate(id, req.body, {new: true})
+            res.status(200).json(user)
+        } catch (error) {
+            res.status(500).json({Message: error.message})
+        }
+    }
+}
