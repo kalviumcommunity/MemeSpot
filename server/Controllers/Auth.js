d0146f54bd6ff99d155752d4cf9ab50d2d18838e
Vighnesh <vighneshzagade4@gmail.com>
2023-07-13 16:55:39 +0530

    Created User Login Functionality

--- server/Controllers/Auth.js
+++ server/Controllers/Auth.js
@@ -1,6 +1,7 @@
 import UserModel from "../Models/UserModel.js";
 import bcrypt from 'bcrypt'
 
+//registerUser
 export const registerUser = async(req, res) => {
     const {
         username, 
@@ -24,5 +25,23 @@ export const registerUser = async(req, res) => {
         res.status(200).json(newUser)
     } catch (err) {
         res.status(500).json({message: err.message})
+    }
+}
+
+//Login user 
+export const loginUser = async(req, res) => {
+    const {username, password} = req.body
+    
+    try {
+        const user = await UserModel.findOne({username: username})    
+
+        if(user){
+            const validate = await bcrypt.compare(password, user.password)
+            validate ? res.status(200).json(user) : res.status(400).json("Wrong Password")
+        } else {
+            res.status(404).json("User Doesn't exists")
+        }
+    } catch (error) {
+            res.status(404).json("User Doesn't exists")
     }
 }
--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { registerUser } from '../Controllers/Auth.js'
+import { registerUser, loginUser } from '../Controllers/Auth.js'
 
 const router = express.Router()
 
 router.post('/register', registerUser)
+router.post('/login', loginUser)
 
 
 export default router
