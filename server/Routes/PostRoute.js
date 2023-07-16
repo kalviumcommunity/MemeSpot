a46a6ac0097670803d96f3897f43470a9bb5c22f
Vighnesh <vighneshzagade4@gmail.com>
2023-07-16 14:37:55 +0530

    Created updatePost request

--- server/Routes/PostRoute.js
+++ server/Routes/PostRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createPost, getPost } from '../Controllers/PostController.js'
+import { createPost, getPost, updatePost } from '../Controllers/PostController.js'
 
 const router = express.Router()
 
 router.post('/create', createPost)
 router.get('/getPost', getPost)
+router.put('/:id', updatePost)
 
 export default router 
